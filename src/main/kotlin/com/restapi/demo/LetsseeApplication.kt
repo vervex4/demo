adeedc8aed35acae3bb5312d13b812adb712f683
support <support@astrobasic.com>
2021-08-08 16:55:18 -0700

    Initial commit

---
+++ src/main/kotlin/com/restapi/demo/LetsseeApplication.kt
@@ -0,0 +1,11 @@
+package com.restapi.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetsseeApplication
+
+fun main(args: Array<String>) {
+	runApplication<LetsseeApplication>(*args)
+}
